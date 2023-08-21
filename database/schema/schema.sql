8e0ad75d0ed56e2cc09888c65307a3db20ef55fe
Magic Cat <pusz.monika@gmail.com>
2023-08-21 15:00:09 +0700

    update token price schema and type

--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -8,8 +8,9 @@ CREATE TABLE token
 CREATE TABLE token_unit
 (
     token_name TEXT NOT NULL REFERENCES token (name),
+    symbol     TEXT NOT NULL,
     denom      TEXT NOT NULL UNIQUE,
-    exponent   INT  NOT NULL,
+    exponent   BIGINT  NOT NULL,
     price_id   TEXT
 );
 
@@ -17,11 +18,23 @@ CREATE TABLE token_unit
 
 CREATE TABLE token_price
 (
-    id         SERIAL                      NOT NULL PRIMARY KEY,
-    unit_name  TEXT                        NOT NULL REFERENCES token_unit (denom) UNIQUE,
-    price      DECIMAL                     NOT NULL,
-    market_cap BIGINT                      NOT NULL,
-    timestamp  TIMESTAMP WITHOUT TIME ZONE NOT NULL
+    id                       SERIAL                      NOT NULL PRIMARY KEY,
+    price_id                 TEXT                        NOT NULL,
+    name                     TEXT                        NOT NULL,
+    image                    TEXT,
+    price                    DECIMAL                     NOT NULL,
+    market_cap               BIGINT                      NOT NULL,
+    market_cap_rank          BIGINT                      NOT NULL,
+    fully_diluted_valuation  TEXT,
+    total_volume             BIGINT                      NOT NULL,
+    high_24h                 BIGINT                      NOT NULL,
+    low_24h                  BIGINT                      NOT NULL,
+    circulating_supply       BIGINT                      NOT NULL,
+    total_supply             BIGINT                      NOT NULL,
+    max_supply               BIGINT                      NOT NULL,
+    ath                      BIGINT                      NOT NULL,
+    atl                      BIGINT                      NOT NULL,
+    timestamp                TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
 CREATE INDEX token_price_timestamp_index ON token_price (timestamp);
 
--- types/token_price.go
+++ types/token_price.go
@@ -4,10 +4,23 @@ import "time"
 
 // MarketTicker contains the current market data for a single token
 type MarketTicker struct {
-	Symbol       string    `json:"symbol"`
-	CurrentPrice float64   `json:"current_price"`
-	MarketCap    float64   `json:"market_cap"`
-	LastUpdated  time.Time `json:"last_updated"`
+	CoingeckoID           string    `json:"id"`
+	Symbol                string    `json:"symbol"`
+	Name                  string    `json:"name"`
+	Image                 string    `json:"image"`
+	CurrentPrice          float64   `json:"current_price"`
+	MarketCap             float64   `json:"market_cap"`
+	MarketCapRank         int64     `json:"market_cap_rank"`
+	FullyDilutedValuation string    `json:"fully_diluted_valuation"`
+	TotalVolume           int64     `json:"total_volume"`
+	High24Hrs             float64   `json:"high_24h"`
+	Low24Hrs              float64   `json:"low_24h"`
+	CirculatingSupply     float64   `json:"circulating_supply"`
+	TotalSupply           float64   `json:"total_supply"`
+	MaxSupply             float64   `json:"max_supply"`
+	ATH                   float64   `json:"ath"`
+	ATL                   float64   `json:"atl"`
+	LastUpdated           time.Time `json:"last_updated"`
 }
 
 // Token contains the information of a single token
@@ -22,18 +35,46 @@ type Tokens []Token
 
 // TokenPrice represents the price of a token unit
 type TokenPrice struct {
-	UnitName  string
-	Price     float64
-	MarketCap int64
-	Timestamp time.Time
+	CoingeckoID           string    `json:"id"`
+	Symbol                string    `json:"symbol"`
+	Name                  string    `json:"name"`
+	Image                 string    `json:"image"`
+	Price                 int64     `json:"price"`
+	MarketCap             int64     `json:"market_cap"`
+	MarketCapRank         int64     `json:"market_cap_rank"`
+	FullyDilutedValuation string    `json:"fully_diluted_valuation"`
+	TotalVolume           int64     `json:"total_volume"`
+	High24Hrs             int64     `json:"high_24h"`
+	Low24Hrs              int64     `json:"low_24h"`
+	CirculatingSupply     int64     `json:"circulating_supply"`
+	TotalSupply           int64     `json:"total_supply"`
+	MaxSupply             int64     `json:"max_supply"`
+	ATH                   int64     `json:"ath"`
+	ATL                   int64     `json:"atl"`
+	Timestamp             time.Time `json:"timestamp"`
 }
 
 // NewTokenPrice creates new TokenPrice instance
-func NewTokenPrice(unitName string, price float64, marketCap int64, timestamp time.Time) TokenPrice {
+func NewTokenPrice(coingeckoID, symbol, name, image string, price, marketCap, marketCapRank int64,
+	fullyDilutedValuation string, totalVolume int64, high24Hrs, low24Hrs, circulatingSupply, totalSupply,
+	maxSupply, ath, atl int64, timestamp time.Time) TokenPrice {
 	return TokenPrice{
-		UnitName:  unitName,
-		Price:     price,
-		MarketCap: marketCap,
-		Timestamp: timestamp,
+		CoingeckoID:           coingeckoID,
+		Symbol:                symbol,
+		Name:                  name,
+		Image:                 image,
+		Price:                 price,
+		MarketCap:             marketCap,
+		MarketCapRank:         marketCapRank,
+		FullyDilutedValuation: fullyDilutedValuation,
+		TotalVolume:           totalVolume,
+		High24Hrs:             high24Hrs,
+		Low24Hrs:              low24Hrs,
+		CirculatingSupply:     circulatingSupply,
+		TotalSupply:           totalSupply,
+		MaxSupply:             maxSupply,
+		ATH:                   ath,
+		ATL:                   atl,
+		Timestamp:             timestamp,
 	}
 }
